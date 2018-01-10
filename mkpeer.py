a945962bc0a59607e8d06c808eecc16a3993d675
Charlie Allom <yeled@github.com>
2018-01-10 12:45:58 +0000

    make yaml dumping work. messy

--- mkpeer.py
+++ mkpeer.py
@@ -3,96 +3,109 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import os
-import yaml
+from ruamel import yaml
 import argparse
 
 PEERINGDB_USERNAME = os.environ.get('PEERINGDB_USERNAME')
 PEERINGDB_PASSWORD = os.environ.get('PEERINGDB_PASSWORD')
 
 
-config = yaml.load(open('config.yaml'))
-pdata = dict()
+config = yaml.load(open('config.yaml'), Loader=yaml.Loader)
 self_asn = str(config['self_asn'])
 ixp = dict()
 ixp_id = dict()
 possible_peers = []
 ixp_choices = []
 noc_email = ''
+new_data = dict()
+list_of_asns = []
+pdata = dict()
 
 for ixp_shortname in config['ixps']:
     ixp_choices.append(ixp_shortname)
 parser = argparse.ArgumentParser(description='Query PeeringDB for peer BGP config')
-parser.add_argument('asn', metavar='ASN', help='peer ASN')
 parser.add_argument('-i', '--ixp', choices=ixp_choices, help='restrict config for matching IXP (default: any found)')
+parser.add_argument('-a', '--asn', metavar='ASN', nargs="+", action='append', help='peer ASN')
 args = parser.parse_args()
 
 
+# https://bitbucket.org/ruamel/yaml/issues/138/possible-to-dump-without-aliases
+class MySD(yaml.SafeDumper):
+    def ignore_aliases(self, _data):
+        return True
+
+
 def main():
+    for peer_asn in args.asn:
+        asns = [self_asn, peer_asn[0]]
+        #print str(type(peer_asn[0])) + str(peer_asn[0]) + " main"
+        ixp_restrict = fetch_ixp_id(args.ixp)
+        for asn in asns:
+            #print asn # print each asn tuple
+            pdata[asn] = pdb(asn)
+            #print yaml.dump(pdata, Dumper=yaml.RoundTripDumper)
+        try:
+            for asn in peer_asn:
+                asn = pdata[str(asn)]['data'][0]['asn']
+        except IndexError:
+            print("# Looks like an empty dataset, exiting")
+            print("result: %s" % pdata[asn])
+            exit(1)
 
-    asns = [self_asn, args.asn]
-    ixp_restrict = fetch_ixp_id(args.ixp)
-    for asn in asns:
-        pdata[asn] = pdb(asn)
-
-    try:
-        for asn in pdata.keys():
-            asn = pdata[str(asn)]['data'][0]['asn']
-    except IndexError:
-        print("# Looks like an empty dataset, exiting")
-        print("result: %s" % pdata[asn])
-        exit(1)
-
-    # Dump all our ix names into a list
-    for asn in pdata.keys():
+        # Dump all our ix names into a list
         ixp[asn] = get_facility_name(pdata[str(asn)], "netixlan_set")
         ixp_id[asn] = get_facility_id(pdata[str(asn)], "netixlan_set")
 
-    # For all our IXs, see if they have the same IX
-    # Have to seed the common ix list with the first entry
-    common_ix_list = ixp[list(ixp)[0]]
-    for asn in ixp:
-        common_ix_list = list(set(ixp[asn]).intersection(common_ix_list))
+        # For all our IXs, see if they have the same IX
+        # Have to seed the common ix list with the first entry
+        common_ix_list = ixp[list(ixp)[0]]
+        for asn in ixp:
+            common_ix_list = list(set(ixp[asn]).intersection(common_ix_list))
 
-    if len(common_ix_list) < 1:
-        print("# Didnt find any common IX, exiting...")
-        exit(1)
-    for asn in pdata.keys():
-        possible_peers.append(pdata[asn]['data'][0]['name'])
-    print '# ' + possible_peers[0] + ' can peer with ' + possible_peers[1] + '!'
+        if len(common_ix_list) < 1:
+            print("# Didnt find any common IX, exiting...")
+            exit(1)
+        for asn in pdata.keys():
+            possible_peers.append(pdata[asn]['data'][0]['name'])
+        #print '#### ' + possible_peers[0] + ' can peer with ' + possible_peers[1] + '!'
 
-    for ix in common_ix_list:
-        if ixp_restrict is None:
-            print_config(ix)
-            continue
-        if ix == ixp_restrict:
-            print_config(ix)
-            continue
+        for ix in common_ix_list:
+            if ixp_restrict is None:
+                print("# exiting...")
+                exit(1)
+            if ix == ixp_restrict:
+                print_config(ix, peer_asn)
+        print yaml.dump(list_of_asns, Dumper=MySD)
 
-
-def print_config(ix):
-    for asn in pdata.keys():
+def print_config(ix, peer_asn):
+    for asn in peer_asn:
+        new_data['inet'] = {}
+        new_data['inet6'] = {}
+        new_data['inet']['peer_ips'] = []
+        new_data['inet6']['peer_ips'] = []
         if self_asn not in asn:  # only work on the peer ASN, skip our own API json
+            #print str(type(asn)) + str(asn)
+            new_data['peer_as'] = asn
+            new_data['name'] = pdata[asn]['data'][0]['name']
             max_prefixes_v4 = pdata[asn]['data'][0]['info_prefixes4']
+            max_prefixes_v6 = pdata[asn]['data'][0]['info_prefixes6']
             for noc_role in pdata[asn]['data'][0]['poc_set']:
                 if 'NOC' in noc_role['role']:
-                    noc_email = noc_role['email']
+                    new_data['contact'] = noc_role['email']
                 elif 'Technical' in noc_role['role']:
-                    noc_email = noc_role['email']
+                    new_data['contact'] = noc_role['email']
                 elif 'Policy' in noc_role['role']:
-                    noc_email = noc_role['email']
-            if override_email(asn) is not None:
-                noc_email = override_email(asn)
+                    new_data['contact'] = noc_role['email']
             for i in pdata[asn]['data'][0]['netixlan_set']:
                 if ix == i['ixlan_id']:
-                    print "# " + i['name']
                     # Skip if ix is not in our shared list.
+                    new_data['inet']['prefix-limit'] = max_prefixes_v4
+                    new_data['inet6']['prefix-limit'] = max_prefixes_v6
                     for entry in ['ix']:
-                        v4 = i['ipaddr4']
-                        print "set protocols bgp group IX-PEERS-V4 neighbor %s peer-as %s" % (v4, i['asn'])
-                        print "set protocols bgp group IX-PEERS-V4 neighbor %s description \"%s:%s:AS%s\"" \
-                              % (v4, noc_email, possible_peers[0], i['asn'])
-                        print "set protocols bgp group IX-PEERS-V4 neighbor %s family inet unicast prefix-limit maximum %s" \
-                              % (v4, max_prefixes_v4)
+                        new_data['inet']['peer_ips'].append(i['ipaddr4'])
+                        new_data['inet6']['peer_ips'].append(i['ipaddr6'])
+            #print yaml.dump(new_data, Dumper=yaml.RoundTripDumper)
+        list_of_asns.append(new_data)
 
 
 def get_facility_name(pdb, nettype):
@@ -119,14 +132,6 @@ def pdb(ASN):
         exit(1)
     pdb_res = r.json()
     return pdb_res
-
-
-def override_email(asn):
-    try:
-        noc_email = config['asn_overrides'][int(asn)]['noc_email']
-        return noc_email
-    except:
-        return None
 
 
 def fetch_ixp_id(ixp_name):
